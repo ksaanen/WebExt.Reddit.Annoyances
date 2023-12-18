104cd7f976b4f176abd957be42c5cfa8c507f172
Kevin Saanen <kevin@saanen.me>
2023-12-18 13:02:19 +0100

    Version 1.1.0 update, unfix header + reject nonessential cookie, options page

---
+++ content_script.js
@@ -0,0 +1,23 @@
+function unfixHeader() {
+    document.querySelector("shreddit-app").classList.add("header-unfix");
+    document.querySelector("reddit-header-small").classList.remove("fixed");
+}
+
+function rejectNonEssential() {
+    const d = new Date();
+    const exdays = 365;
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    document.cookie = `eu_cookie={%22opted%22:true%2C%22nonessential%22:false}; expires=${d.toUTCString()}; SameSite=None; Secure`;
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function onGot(result) {
+    if (result.unfixHeader) unfixHeader();
+    if (result.rejectNonEssential) rejectNonEssential();
+}
+
+const getting = browser.storage.sync.get();
+getting.then(onGot, onError);
